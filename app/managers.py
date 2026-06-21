8d7a567a571ee6c01b01086e864ba8b822c78fe5
Andrii Hrabovskiy <yojyk.pl@gmail.com>
2026-06-21 08:31:52 +0200

    Add main.py, fix update params order

--- app/managers.py
+++ app/managers.py
@@ -4,15 +4,16 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self.conn.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name)
         )
         self.conn.commit()
@@ -25,14 +26,15 @@ class ActorManager:
             Actor(*row) for row in result_cursor
         ]
 
-    def update(self, new_first_name: str, new_last_name: str, pk: int):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.conn.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} SET first_name=?,"
+            f" last_name=? WHERE id=?",
             (new_first_name, new_last_name, pk)
         )
         self.conn.commit()
 
-    def delete(self, pk: int):
+    def delete(self, pk: int) -> None:
         self.conn.execute(
             f"DELETE FROM {self.table_name} WHERE id=?",
             (pk, )
